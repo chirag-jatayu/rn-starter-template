685edb403dda0806996b5077fff114aa85f3360a
chirag-jatayu <chirag.jatayu@gmail.com>
2025-06-06 12:28:21 +0530

    first commit

--- src/navigation/AppNav.tsx
+++ src/navigation/AppNav.tsx
@@ -1,12 +1,12 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
-const App = () => {
+const AppNav = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>AppNav</Text>
     </View>
   );
 };
 
-export default App;
+export default AppNav;
